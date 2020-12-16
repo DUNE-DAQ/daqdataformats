6bd2c84ae3da7aeb6c16b5b242f2ae17e9d73eea
Eric Flumerfelt <eflumerf@fnal.gov>
2020-12-16 14:55:10 -0600

    Change vector<uint8_t> to void* with explicit malloc in constructor. Add get_storage_location and "existing Fragment" constructor for DataFlow. Add test for "existing Fragment" constructor. Update UML diagram.

--- include/dataformats/Fragment.hpp
+++ include/dataformats/Fragment.hpp
@@ -35,25 +35,35 @@ public:
     size_t size = sizeof(FragmentHeader) +
                   std::accumulate(pieces.begin(), pieces.end(), 0ULL, [](auto& a, auto& b) { return a + b.second; });
 
-    data_arr_ = std::vector<uint8_t>(size); // NOLINT(build/unsigned)
+    data_arr_ = malloc(size); // NOLINT(build/unsigned)
 
     FragmentHeader header;
     header.size = size;
-    memcpy(&data_arr_[0], &header, sizeof(header));
+    memcpy(data_arr_, &header, sizeof(header));
 
     size_t offset = sizeof(FragmentHeader);
     for (auto& piece : pieces) {
-      memcpy(&data_arr_[0] + offset, piece.first, piece.second); // NOLINT(build/unsigned)
+      memcpy(static_cast<uint8_t*>(data_arr_) + offset, piece.first, piece.second); // NOLINT(build/unsigned)
       offset += piece.second;
     }
   }
   Fragment(void* buffer, size_t size)
     : Fragment({ std::make_pair(buffer, size) })
   {}
+  explicit Fragment(void* existing_fragment_buffer, bool copy_from_buffer = false)
+  {
+    if (!copy_from_buffer) {
+        data_arr_ = existing_fragment_buffer;
+    } else {
+      auto header = reinterpret_cast<FragmentHeader*>(existing_fragment_buffer); // NOLINT
+      data_arr_ = malloc(header->size);
+      memcpy(data_arr_, existing_fragment_buffer, header->size);
+    }
+  }
 
   FragmentHeader const& get_header() { return *header_(); }
-  void set_header(FragmentHeader header) { memcpy(&data_arr_[0], &header, sizeof(header)); }
-  uint8_t* get_storage_location() { return &data_arr_[0]; }
+  void set_header(FragmentHeader header) { memcpy(data_arr_, &header, sizeof(header)); }
+  void* get_storage_location() { return data_arr_; }
 
   // Header setters and getters
   trigger_number_t get_trigger_number() { return header_()->trigger_number; }
@@ -85,15 +95,17 @@ public:
   /**
    * @brief Get the total size of the Fragment
    * @return The size of the Fragment, including header and all payload pieces
-  */
+   */
   fragment_size_t get_size() { return header_()->size; }
-  void* data() { 
+  void* data()
+  {
     // Increment header pointer by one to skip header
-    return static_cast<void*>(header_() + 1);  // NOLINT
+    return static_cast<void*>(header_() + 1); // NOLINT
   }
+
 private:
-  FragmentHeader* header_() { return reinterpret_cast<FragmentHeader*>(&data_arr_[0]); } // NOLINT
-  std::vector<uint8_t> data_arr_; // NOLINT(build/unsigned)
+  FragmentHeader* header_() { return static_cast<FragmentHeader*>(data_arr_); } // NOLINT
+  void* data_arr_;                                                        // NOLINT(build/unsigned)
 };
 } // namespace dataformats
 } // namespace dunedaq
--- unittest/Fragment_test.cxx
+++ unittest/Fragment_test.cxx
@@ -26,4 +26,30 @@ BOOST_AUTO_TEST_CASE(CopyAndMoveSemantics)
   BOOST_REQUIRE(std::is_move_assignable_v<Fragment>);
 }
 
+BOOST_AUTO_TEST_CASE(ExistingFragmentConstructor)
+{
+  FragmentHeader header;
+  header.size = sizeof(FragmentHeader);
+  header.trigger_number = 1;
+  header.trigger_timestamp = 2;
+  header.run_number = 3;
+  auto frag = malloc(sizeof(FragmentHeader));
+  memcpy(frag, &header, sizeof(FragmentHeader));
+  auto testFrag = Fragment(frag); // frag memory now owned by Fragment
+
+  BOOST_REQUIRE_EQUAL(testFrag.get_storage_location(), frag);
+  
+  BOOST_REQUIRE_EQUAL(testFrag.get_trigger_number(), 1);
+  BOOST_REQUIRE_EQUAL(testFrag.get_trigger_timestamp(), 2);
+  BOOST_REQUIRE_EQUAL(testFrag.get_run_number(), 3);
+
+  testFrag = Fragment(frag, true);
+
+  BOOST_REQUIRE(testFrag.get_storage_location() != frag);
+  BOOST_REQUIRE_EQUAL(testFrag.get_trigger_number(), 1);
+  BOOST_REQUIRE_EQUAL(testFrag.get_trigger_timestamp(), 2);
+  BOOST_REQUIRE_EQUAL(testFrag.get_run_number(), 3);
+
+}
+
 BOOST_AUTO_TEST_SUITE_END()
