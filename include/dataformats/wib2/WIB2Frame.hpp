342abc78219d17802b3ea5b46b47dfb048f0f420
Philip Rodrigues <rodriges@fnal.gov>
2021-02-25 11:55:17 +0000

    Code clean up
    * Use s_ prefix for static constants
    * Use more named static constants in place of hardcoded values
    * Move ADC index calculation to its own function instead of repeating it

--- include/dataformats/wib2/WIB2Frame.hpp
+++ include/dataformats/wib2/WIB2Frame.hpp
@@ -26,8 +26,14 @@ public:
   // The definition of the format is in terms of 32-bit words
   typedef uint32_t word_t;
 
-  static const int BITS_PER_ADC = 14;
-  static const int BITS_PER_WORD = 8 * sizeof(word_t);
+  static constexpr int s_bits_per_adc = 14;
+  static constexpr int s_bits_per_word = 8 * sizeof(word_t);
+  static constexpr int s_num_channels = 256;
+  static constexpr int s_num_adc_words = s_num_channels * s_bits_per_adc / s_bits_per_word;
+  static constexpr int s_u_channels_per_femb = 40;
+  static constexpr int s_v_channels_per_femb = 40;
+  static constexpr int s_x_channels_per_femb = 48;
+  static constexpr int s_channels_per_femb = s_u_channels_per_femb + s_v_channels_per_femb + s_x_channels_per_femb;
 
   struct Header
   {
@@ -50,7 +56,7 @@ public:
   // Data members
   // ===============================================================
   Header header;
-  word_t adc_words[112];
+  word_t adc_words[s_num_adc_words];
   Trailer trailer;
 
   // ===============================================================
@@ -71,20 +77,20 @@ public:
    */
   uint16_t get_adc(int i) const
   {
-    if (i < 0 || i > 255)
+    if (i < 0 || i >= s_num_channels)
       throw std::out_of_range("ADC index out of range");
 
     // The index of the first (and sometimes only) word containing the required ADC value
-    int word_index = BITS_PER_ADC * i / BITS_PER_WORD;
-    assert(word_index < 112);
+    int word_index = s_bits_per_adc * i / s_bits_per_word;
+    assert(word_index < s_num_adc_words);
     // Where in the word the lowest bit of our ADC value is located
-    int first_bit_position = (BITS_PER_ADC * i) % BITS_PER_WORD;
+    int first_bit_position = (s_bits_per_adc * i) % s_bits_per_word;
     // How many bits of our desired ADC are located in the `word_index`th word
-    int bits_from_first_word = std::min(BITS_PER_ADC, BITS_PER_WORD - first_bit_position);
+    int bits_from_first_word = std::min(s_bits_per_adc, s_bits_per_word - first_bit_position);
     uint16_t adc = adc_words[word_index] >> first_bit_position;
     // If we didn't get the full 14 bits from this word, we need the rest from the next word
-    if (bits_from_first_word < BITS_PER_ADC) {
-      assert(word_index + 1 < 112);
+    if (bits_from_first_word < s_bits_per_adc) {
+      assert(word_index + 1 < s_num_adc_words);
       adc |= adc_words[word_index + 1] << bits_from_first_word;
     }
     // Mask out all but the lowest 14 bits;
@@ -92,26 +98,26 @@ public:
   }
 
   /**
-   * @brief Get the ith ADC value in the frame to val
+   * @brief Set the ith ADC value in the frame to @p val
    */
   void set_adc(int i, uint16_t val)
   {
-    if (i < 0 || i > 255)
+    if (i < 0 || i >= s_num_channels)
       throw std::out_of_range("ADC index out of range");
-    if (val >= (1 << 14))
+    if (val >= (1 << s_bits_per_adc))
       throw std::out_of_range("ADC value out of range");
 
     // The index of the first (and sometimes only) word containing the required ADC value
-    int word_index = BITS_PER_ADC * i / BITS_PER_WORD;
-    assert(word_index < 112);
+    int word_index = s_bits_per_adc * i / s_bits_per_word;
+    assert(word_index < s_num_adc_words);
     // Where in the word the lowest bit of our ADC value is located
-    int first_bit_position = (BITS_PER_ADC * i) % BITS_PER_WORD;
+    int first_bit_position = (s_bits_per_adc * i) % s_bits_per_word;
     // How many bits of our desired ADC are located in the `word_index`th word
-    int bits_in_first_word = std::min(BITS_PER_ADC, BITS_PER_WORD - first_bit_position);
+    int bits_in_first_word = std::min(s_bits_per_adc, s_bits_per_word - first_bit_position);
     adc_words[word_index] |= (val << first_bit_position);
     // If we didn't put the full 14 bits in this word, we need to put the rest in the next word
-    if (bits_in_first_word < BITS_PER_ADC) {
-      assert(word_index + 1 < 112);
+    if (bits_in_first_word < s_bits_per_adc) {
+      assert(word_index + 1 < s_num_adc_words);
       adc_words[word_index + 1] |= val >> bits_in_first_word;
     }
   }
@@ -120,82 +126,86 @@ public:
    */
   uint16_t get_u(int femb, int i) const
   {
-    if (femb < 0 || femb > 1)
-      throw std::out_of_range("FEMB index out of range");
-    if (i < 0 || i >= 40)
-      throw std::out_of_range("U channel index out of range");
-
-    return get_adc(128 * femb + i);
+    return get_adc(get_adc_index(kU, femb, i));
   }
 
   /** @brief Get the ith V-channel ADC in the given femb
    */
   uint16_t get_v(int femb, int i) const
   {
-    if (femb < 0 || femb > 1)
-      throw std::out_of_range("FEMB index out of range");
-    if (i < 0 || i >= 40)
-      throw std::out_of_range("V channel index out of range");
-
-    return get_adc(128 * femb + 40 + i);
+    return get_adc(get_adc_index(kV, femb, i));
   }
 
   /** @brief Get the ith X-channel (ie, collection) ADC in the given femb
    */
   uint16_t get_x(int femb, int i) const
   {
-    if (femb < 0 || femb > 1)
-      throw std::out_of_range("FEMB index out of range");
-    if (i < 0 || i >= 48)
-      throw std::out_of_range("X channel index out of range");
-
-    return get_adc(128 * femb + 40 + 40 + i);
+    return get_adc(get_adc_index(kX, femb, i));
   }
 
   /** @brief Set the ith U-channel ADC in the given femb to val
    */
   void set_u(int femb, int i, uint16_t val)
   {
-    if (femb < 0 || femb > 1)
-      throw std::out_of_range("FEMB index out of range");
-    if (i < 0 || i >= 40)
-      throw std::out_of_range("U channel index out of range");
-
-    return set_adc(128 * femb + i, val);
+    return set_adc(get_adc_index(kU, femb, i), val);
   }
 
   /** @brief Set the ith V-channel ADC in the given femb to val
    */
   void set_v(int femb, int i, uint16_t val)
   {
-    if (femb < 0 || femb > 1)
-      throw std::out_of_range("FEMB index out of range");
-    if (i < 0 || i >= 40)
-      throw std::out_of_range("V channel index out of range");
-
-    return set_adc(128 * femb + 40 + i, val);
+    return set_adc(get_adc_index(kV, femb, i), val);
   }
 
   /** @brief Set the ith X-channel (ie, collection) ADC in the given femb to val
    */
   void set_x(int femb, int i, uint16_t val)
   {
-    if (femb < 0 || femb > 1)
-      throw std::out_of_range("FEMB index out of range");
-    if (i < 0 || i >= 48)
-      throw std::out_of_range("X channel index out of range");
-
-    return set_adc(128 * femb + 40 + 40 + i, val);
+    return set_adc(get_adc_index(kX, femb, i), val);
   }
 
   /** @brief Get the 64-bit timestamp of the frame
    */
   uint64_t get_timestamp() const { return (uint64_t)header.timestamp_1 | ((uint64_t)header.timestamp_2 << 32); }
-};
 
-// Define the static constants from WIB2Frame to avoid a linker error
-const int WIB2Frame::BITS_PER_ADC;
-const int WIB2Frame::BITS_PER_WORD;
+private:
+
+  enum View {
+    kU, kV, kX
+  };
+
+  int get_adc_index(View view, int femb, int i) const
+  {
+    if (femb < 0 || femb > 1){
+      throw std::out_of_range("FEMB index out of range");
+    }
+
+    int offset=0;
+    int n_channels=0;
+
+    switch(view){
+    case kU:
+      offset=0;
+      n_channels=s_u_channels_per_femb;
+      break;
+    case kV:
+      offset=s_u_channels_per_femb;
+      n_channels=s_v_channels_per_femb;
+      break;
+    case kX:
+      offset=s_u_channels_per_femb+s_v_channels_per_femb;
+      n_channels=s_x_channels_per_femb;
+      break;
+    }
+
+    if (i < 0 || i >= n_channels){
+      throw std::out_of_range("Channel index out of range");
+    }
+
+    return s_channels_per_femb*femb + offset + i;
+  }
+
+};
 
 }
 
