6ddfa76db8798ca803ed0e51219743fc90cc6850
Eric Flumerfelt <eflumerf@fnal.gov>
2021-06-24 07:23:59 -0500

    Update version number of TriggerRecordHeaderData and FragmentHeader

--- include/dataformats/TriggerRecordHeaderData.hpp
+++ include/dataformats/TriggerRecordHeaderData.hpp
@@ -34,7 +34,7 @@ struct TriggerRecordHeaderData
   /**
    * @brief The current version of the TriggerRecordHeader
    */
-  static constexpr uint32_t s_trigger_record_header_version = 1; // NOLINT(build/unsigned)
+  static constexpr uint32_t s_trigger_record_header_version = 2; // NOLINT(build/unsigned)
 
   /**
    * @brief An invalid number of components
