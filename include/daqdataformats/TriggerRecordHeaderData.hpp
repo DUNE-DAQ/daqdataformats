d4145e79468ada093488057ee5694ef42fef3692
MRiganSUSX <M.Rigan@sussex.ac.uk>
2024-03-25 21:47:54 +0100

    ctb integration: another pass

--- include/daqdataformats/TriggerRecordHeaderData.hpp
+++ include/daqdataformats/TriggerRecordHeaderData.hpp
@@ -99,7 +99,7 @@ struct TriggerRecordHeaderData
   /**
    * @brief Padding to ensure 64-bit alignment
    */
-  uint32_t unused{ 0xFFFF }; // NOLINT(build/unsigned)
+  uint32_t unused{ 0xFFFFFFFF }; // NOLINT(build/unsigned)
 
   SourceID element_id;
 };
